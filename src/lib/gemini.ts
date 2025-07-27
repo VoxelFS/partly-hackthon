d5e07bf0b37cbe1a802166738ee0a6b331002ac4
Taks07 <yutakaoishi7@gmail.com>
2025-07-27 13:29:19 +1200

    working prompt

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -2,6 +2,116 @@ import {createPartFromUri, createUserContent, GoogleGenAI} from "@google/genai";
 import path from "path";
 import fs from "fs/promises";
 
+const prompt =`
+Describe whether or not parts of the car are damaged. Rate each part as "As new", A, B, and C. Use the provided JSON of car parts below. Return the exact same JSON with the field name "grade" corresponding to the part rating. If the part is not in the image, or the part is not clear, set the grade as null.
+
+Part JSON:
+[
+  {
+    "name": "Mid Cut",
+    "image": "images/mid_cut.jpg",
+    "parts": [
+      {
+        "name": "Door Mirrors",
+        "image": "images/door_mirrors.jpg",
+        "parts": [
+          {
+            "name": "Door Mirror, Left",
+            "image": "images/door_mirror_left.jpg",
+            "parts": []
+          },
+          {
+            "name": "Door Mirror, Right",
+            "image": "images/door_mirror_right.jpg",
+            "parts": []
+          }
+        ]
+      },
+      {
+        "name": "Front Doors",
+        "image": "images/front_doors.jpg",
+        "parts": [
+          {
+            "name": "Door, Front Left",
+            "image": "images/door_front_left.jpg",
+            "parts": []
+          },
+          {
+            "name": "Door, Front Right",
+            "image": "images/door_front_right.jpg",
+            "parts": []
+          }
+        ]
+      },
+      {
+        "name": "Rear Doors",
+        "image": "images/rear_doors.jpg",
+        "parts": [
+          {
+            "name": "Door, Rear Left",
+            "image": "images/door_rear_left.jpg",
+            "parts": []
+          },
+          {
+            "name": "Door, Rear Right",
+            "image": "images/door_rear_right.jpg",
+            "parts": []
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "name": "Front Cut",
+    "image": "images/front_cut.jpg",
+    "parts": [
+      {
+        "name": "Headlights",
+        "image": "images/headlights.jpg",
+        "parts": [
+          {
+            "name": "Headlight, Left",
+            "image": "images/headlight_left.jpg",
+            "parts": []
+          },
+          {
+            "name": "Headlight, Right",
+            "image": "images/headlight_right.jpg",
+            "parts": []
+          }
+        ]
+      },
+      {
+        "name": "Front Bumper",
+        "image": "images/front_bumper.jpg",
+        "parts": []
+      },
+      {
+        "name": "Hood",
+        "image": "images/hood.jpg",
+        "parts": []
+      },
+      {
+        "name": "Front Fenders",
+        "image": "images/front_fenders.jpg",
+        "parts": [
+          {
+            "name": "Fender, Left",
+            "image": "images/fender_left.jpg",
+            "parts": []
+          },
+          {
+            "name": "Fender, Right",
+            "image": "images/fender_right.jpg",
+            "parts": []
+          }
+        ]
+      }
+    ]
+  }
+]
+`
+
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY })
 
 export default async function analyzeImageWithGemini(filePath: string) {
@@ -15,17 +125,12 @@ export default async function analyzeImageWithGemini(filePath: string) {
 
     const fileContents = await fs.readFile(fPath, 'utf8');
     const data = JSON.parse(fileContents);
-    console.log(data);
 
     const result = await ai.models.generateContent({
         model: "gemini-2.5-flash",
         contents: createUserContent([
             createPartFromUri(myfile.uri || "", myfile.mimeType || ""),
-            "\n\n",
-            "Describe whether or not parts of the car are damaged. Rate each part as \"As new\", A, B, and C. Use the provided JSON of car parts below. Return the exact same JSON with the field name \"grade\" corresponding to the part rating. If the part is not in the image, or the part is not clear, set the grade as null.\n" +
-            "\n" +
-            "Part JSON:" +
-            data
+            prompt
         ]),
     });
     console.log(result.text);
